0986601acec41244b05ffefa53961bf96c1a46fa
MasonRowley <Row12006@byui.edu>
2019-03-12 23:00:55 -0600

    images fixed

--- assignments/lesson-9/scripts/town-data.js
+++ assignments/lesson-9/scripts/town-data.js
@@ -18,11 +18,12 @@
         showSodaInfo(towninfo);
     }
 
+ 
   
 
 function showPrestonTownInfo(jsonObj) {
   var towns = jsonObj['towns'];
-  
+  console.log(towns);
   
 
     if( towns.name = "Preston") {
@@ -33,14 +34,18 @@ function showPrestonTownInfo(jsonObj) {
     var myPara2 = document.createElement('p');
     var myPara3 = document.createElement('p');
     var myPara4 = document.createElement('p');
+    var img = document.createElement("img");
+    img.setAttribute("src","images/preston-home.png");
+    img.setAttribute("alt","Preston Picture");
+    img.setAttribute("srcset","images/preston-home-small.png 350w");
+    //var src = document.getElementById("article")
 
     myH2.textContent = towns.name;
     myPara1.textContent = 'Town Motto:' + " " + towns[4].motto;
     myPara2.textContent = 'Year Founded:' + " " + towns[4].yearFounded;
     myPara3.textContent = 'Average Rainfall:' + " " + towns[4].averageRainfall;
-    myPara4.textContent = 'Current Population:' + " " + towns[4].currentPopulation;     
+    myPara4.textContent = 'Current Population:' + " " + towns[4].currentPopulation; 
     
-
     section.appendChild(myArticle);
 
     myArticle.appendChild(myH2);
@@ -48,8 +53,11 @@ function showPrestonTownInfo(jsonObj) {
     myArticle.appendChild(myPara2);
     myArticle.appendChild(myPara3);
     myArticle.appendChild(myPara4);
+    myArticle.appendChild(img);
     }
     }
+
+
     function showFishHavenTownInfo(jsonObj) {
         var towns = jsonObj['towns'];
         
@@ -63,6 +71,10 @@ function showPrestonTownInfo(jsonObj) {
           var myPara2 = document.createElement('p');
           var myPara3 = document.createElement('p');
           var myPara4 = document.createElement('p');
+          var img = document.createElement('img');
+          img.setAttribute("src","images/fishhaven-home.jpg");
+        img.setAttribute("alt","Fish Haven Picture");
+        img.setAttribute("srcset","images/fishhaven-home-small.jpg 350w");
       
           myH2.textContent = towns.name;
           myPara1.textContent = 'Town Motto:' + " " + towns[1].motto;
@@ -78,6 +90,7 @@ function showPrestonTownInfo(jsonObj) {
           myArticle.appendChild(myPara2);
           myArticle.appendChild(myPara3);
           myArticle.appendChild(myPara4);
+          myArticle.appendChild(img);
       
           }
         }
@@ -95,6 +108,10 @@ function showPrestonTownInfo(jsonObj) {
               var myPara2 = document.createElement('p');
               var myPara3 = document.createElement('p');
               var myPara4 = document.createElement('p');
+              var img = document.createElement('img');
+          img.setAttribute("src","images/sodasprings-home.jpg");
+        img.setAttribute("alt","Soda Springs Picture");
+        img.setAttribute("srcset","images/sodasprings-home-small.jpg")
           
               myH2.textContent = towns.name;
               myPara1.textContent = 'Town Motto:' + " " + towns[5].motto;
@@ -110,6 +127,7 @@ function showPrestonTownInfo(jsonObj) {
               myArticle.appendChild(myPara2);
               myArticle.appendChild(myPara3);
               myArticle.appendChild(myPara4);
+              myArticle.appendChild(img);
           
               }
             }
